bc8157c14e4713656162ed9143f104df61a64e46
Valentim Vieira de Araújo <valentim_araujo@yahoo.com.br>
2020-10-01 09:11:44 -0300

    github workflow

---
+++ src/server.ts
@@ -0,0 +1,49 @@
+import './util/module-alias';
+import { Server } from '@overnightjs/core';
+import bodyParser from 'body-parser';
+import { ForecastController } from '@src/controllers/forecast';
+import { Application } from 'express';
+import * as database from '@src/database';
+import { BeachesController } from '@src/controllers/beaches';
+import { UsersController } from '@src/controllers/users';
+
+export class SetupServer extends Server {
+  constructor(private port = 3000) {
+    super();
+  }
+
+  public async init(): Promise<void> {
+    this.setupExpress();
+    this.setupControllers();
+    await this.setupDatabase();
+  }
+
+  private setupExpress(): void {
+    this.app.use(bodyParser.json());
+  }
+
+  private setupControllers(): void {
+    const forecastController = new ForecastController();
+    const beachesController = new BeachesController();
+    const usersController = new UsersController();
+    this.addControllers([forecastController, beachesController, usersController]);
+  }
+
+  private async setupDatabase(): Promise<void> {
+    await database.connect();
+  }
+
+  public getApp(): Application {
+    return this.app;
+  }
+
+  public async close(): Promise<void> {
+    await database.close();
+  }
+
+  public start(): void {
+    this.app.listen(this.port, () => {
+      console.info('Server listening of port:', this.port);
+    });
+  }
+}
